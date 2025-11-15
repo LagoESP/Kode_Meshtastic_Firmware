0d0c2d6d807e0d1e40c6e3ed8f6a3d772ac8cd2a
Jose Lago <jose@lago.dev>
2025-11-15 17:23:43 +0100

    Fix include statement formatting and enhance debug logging in lateInitVariant for Kode Dot tracker

--- src/platform/extra_variants/kode_dot/variant.cpp
+++ src/platform/extra_variants/kode_dot/variant.cpp
@@ -3,12 +3,13 @@
 #ifdef KODE_DOT
 
 
-#include <kode_dot_utils/kode_dot_screen.h>
+#include "kode_dot_utils/kode_dot_screen.h"
 
 void lateInitVariant()
 {
     LOG_DEBUG("Kode Dot tracker lateInitVariant");
     startDisplayTask();
+    LOG_DEBUG("Kode Dot tracker lateInitVariant done");
 }
 
 #endif
